0806d267a8ff8525241404fe9bf656a076585cae
Chris <13677134+devcshort@users.noreply.github.com>
2024-03-10 21:39:00 -0700

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
 import { useState, useEffect } from "react";
-import { listen } from '@tauri-apps/api/event';
+import { UnlistenFn, listen } from "@tauri-apps/api/event";
 
 function App() {
   const [isDraggingFile, setIsDraggingFile] = useState(false);
 
   useEffect(() => {
-    listen("tauri://file-drop", () => {
-      setIsDraggingFile(false);
-    })
+    let unlisten: UnlistenFn = () => {};
+    (async () => {
+      unlisten = await listen("tauri://file-drop", () => {
+        setIsDraggingFile(false);
+      });
+    })();
 
-    listen("tauri://file-drop-hover", () => {
-      setIsDraggingFile(true);
-    });
+    return () => {
+      unlisten();
+    };
+  }, []);
 
-    listen("tauri://file-drop-cancelled", () => {
-      setIsDraggingFile(false);
-    });
+  useEffect(() => {
+    let unlisten: UnlistenFn = () => {};
+    (async () => {
+      unlisten = await listen("tauri://file-drop-hover", () => {
+        setIsDraggingFile(true);
+      });
+    })();
+
+    return () => {
+      unlisten();
+    };
+  }, []);
+
+  useEffect(() => {
+    let unlisten: UnlistenFn = () => {};
+    (async () => {
+      unlisten = await listen("tauri://file-drop-cancelled", () => {
+        setIsDraggingFile(false);
+      });
+    })();
+
+    return () => {
+      unlisten();
+    };
   }, []);
 
   return (
-    <div className={`bg-slate-700 h-screen flex items-center justify-center ${isDraggingFile ? "border-4 border-dashed border-cyan-500" : ""}`}>
-      <p className="text-white text-2xl">Drag a file to this window to get started</p>
+    <div
+      className={`bg-slate-700 h-screen flex items-center justify-center ${
+        isDraggingFile ? "border-4 border-dashed border-cyan-500" : ""
+      }`}
+    >
+      <p className="text-white text-2xl">
+        Drag an image to this window to get started
+      </p>
     </div>
   );
 }
